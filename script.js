b6988a04c9423025dc1d80c63483bfbf6041afac
Manuella Barros <manubarrossa@gmail.com>
2022-03-17 15:35:31 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function Enviar() {
+
+    var nome = document.getElementsByName("name");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
+
+function Enviar()
+
