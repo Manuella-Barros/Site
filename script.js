78b38f55402ba122ee552922481ab8547e35da0b
Manuella Barros <manubarrossa@gmail.com>
2022-03-31 16:53:56 -0300

    Botão funcionando
    Aprendi como usa o botão junto ao js, e consigo armazenar as informações

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function Enviar() {
-
-    var nome = document.getElementsByName("name");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
+function enviar(){
+    var nome = window.document.getElementById("usuario_nome")
+    var celular = window.document.getElementById("usuario_celular")
+    var email = window.document.getElementById("usuario_email")
+    res.innerHTML = `Prazer em te conhecer ${nome.value}! seus dados foram encaminhados com sucesso!, em caso de emergência, entraremos em contato com seu email (${email.value}) ou celular (${celular.value})`
 }
 
-function Enviar()
+
 
