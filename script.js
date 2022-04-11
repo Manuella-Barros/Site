d264f05f688df913c2586409389be538f34079dd
Manuella Barros <manubarrossa@gmail.com>
2022-04-11 09:32:53 -0300

    Preenchimento obrigatório
    O preenchimento dos "input" agora é obrigatório, tirei os "required" do html por que não estava funcionando.
    Agora estou pesquisando sobre como fazer com que o campo de número seja preenchido somente com números, e a parte do email seja preciso ter a palavra "gmail.com"

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ function enviar(){
     var nome = window.document.getElementById("usuario_nome")
     var celular = window.document.getElementById("usuario_celular")
     var email = window.document.getElementById("usuario_email")
-    res.innerHTML = `Prazer em te conhecer ${nome.value}! seus dados foram encaminhados com sucesso!, em caso de emergência, entraremos em contato com seu email (${email.value}) ou celular (${celular.value})`
+    var idade = window.document.getElementById("usuario_aniversario")
+
+    if (nome.value.length == 0 || celular.value.length == 0 || email.value.length == 0 || idade.value.length == 0){
+        alert("Por favor preencha os campos abaixo")
+    } else {
+        res.innerHTML = `Prazer em te conhecer ${nome.value}! seus dados foram encaminhados com sucesso!, em caso de emergência, entraremos em contato com seu email (${email.value}) ou celular (${celular.value})`
+    }
 }
+
 
 
 
